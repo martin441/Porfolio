e3067186acee4e6c7ca3624de266e24397369a2e
Martin Machado <martinmachado441@gmail.com>
2023-05-24 13:21:17 -0300

    instalacion de material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
@@ -9,14 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Button variant="contained">Hello World</Button>
       </header>
     </div>
   );
